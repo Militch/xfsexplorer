ff78f1b77fccb1339fa68cc85306aa5e36a0365a
lichao <lichao@dsyun.io>
2021-10-08 21:04:10 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var {
+  calcBlockReward,
+  coinFormat,
+  baseIntFormat,
+  calcDifficulty,
+} = require('../util/util.js');
+var HttpJsonRpcClient = require('../jsonrpc/client.js');
+
+var cli = new HttpJsonRpcClient({url:'http://127.0.0.1:9001'});
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(req.path);
-  console.log(req.route);
-  console.log(req.baseUrl);
-  res.render('index', { title: 'Home - XFS Explorer',req: req  });
+router.get('/', async function(req, res, next) {
+  var latestHead = await cli.call({method:'Chain.Head'});
+  var currentDifficulty = 1;
+  if (latestHead.height !== 0) {
+    currentDifficulty = calcDifficulty(4278190109, latestHead.bits).toNumber();
+  }
+  console.log('diff:', currentDifficulty);
+  var modelmap = {
+    latestHeight: baseIntFormat(latestHead.height),
+    blockReward: coinFormat(calcBlockReward(latestHead.height)),
+    currentDifficulty: currentDifficulty
+  }
+  res.render('index', { title: 'Home - XFS Explorer',req: req, data: modelmap});
 });
 
 module.exports = router;
--- routes/txs.js
+++ routes/txs.js
@@ -2,9 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  console.log(req.path);
-  console.log(req.route);
-  console.log(req.baseUrl);
   res.render('txs', { title: 'Txs - XFS Explorer',req: req });
 });
 
